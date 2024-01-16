6cd7c1a3bf6f56b1e410faaf33211c9fa97d584a
Ryan Hendriks <ryanhendriks@live.nl>
2024-01-16 12:11:52 +0100

    Beginning react: Episode 1 & int commit

---
+++ src/Another.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Another(props) {
+    return (
+        <div>
+        <h1>Another component! name: {props.name}</h1>
+        </div>
+    );
+}
