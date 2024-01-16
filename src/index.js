84682eedc4e4803170bc04f55d59df57d2e4362f
Ryan Hendriks <ryanhendriks@live.nl>
2024-01-16 16:38:59 +0100

    fixed syntax for react 18 support

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import AppClass from './components/AppClass';
+// import AppClass from './components/AppClass';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+
+// Use createRoot from "react-dom/client" in React 18
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
     <React.StrictMode>
         <App />
-        <AppClass />
+        {/*<AppClass />*/}
     </React.StrictMode>,
-    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
