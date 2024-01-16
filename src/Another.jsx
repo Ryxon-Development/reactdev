f4f7d7d5eb1644a6428c6f932a027be807c3cc0d
Ryan Hendriks <ryanhendriks@live.nl>
2024-01-16 12:40:03 +0100

    prettier plugin installed and reformatting applied
    Episode 2: A better development experience

--- src/Another.jsx
+++ src/Another.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Another(props) {
     return (
         <div>
-        <h1>Another component! name: {props.name}</h1>
+            <h1>Another component! name: {props.name}</h1>
         </div>
     );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -5,52 +5,41 @@ import './App.css';
 import Another from './Another';
 
 function App() {
+    const name = 'Ryan';
 
-  const name = 'Ryan';
+    const [count, setCount] = useState(0);
 
-  const [count, setCount] = useState(0);
+    const someStyle = {
+        color: 'red',
+        fontSize: '40px',
+        backgroundColor: 'yellow',
+        fontWeight: 'bold',
+    };
 
-  const someStyle = {
-    color: 'red',
-    fontSize: '40px',
-    backgroundColor: 'yellow',
-    fontWeight: 'bold'
-  }
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Another name={name} />
 
-  return (
-    <div className="App">
-      <header className="App-header">
+                <div className="counter">
+                    <span>Count: {count}</span>
+                    <div>
+                        <button onClick={() => setCount(count - 1)}>Subtract</button>
+                        <button onClick={() => setCount(count + 1)}>Add</button>
+                    </div>
+                </div>
 
-        <Another name={name}/>
-
-        <div className="counter">
-          <span>Count: {count}</span>
-          <div>
-            <button onClick={() => setCount(count + 1)}>Add</button>
-            <button onClick={() => setCount(count - 1)}>Subtract</button>
-          </div>
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                {true ? <p style={someStyle}>{3 + 3}</p> : <p>False</p>}
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
         </div>
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {
-          true
-            ? <p style={someStyle}>{3 + 3}</p>
-              : <p>False</p>
-        }
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    );
 }
 
 export default App;
