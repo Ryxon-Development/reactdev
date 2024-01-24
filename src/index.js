470bffc19a0d17c8d790c21e65736b275135b035
Ryan Hendriks <ryanhendriks@live.nl>
2024-01-24 13:56:32 +0100

    tailwind + stylings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import Root from './components/Root';
 import reportWebVitals from './reportWebVitals';
 
